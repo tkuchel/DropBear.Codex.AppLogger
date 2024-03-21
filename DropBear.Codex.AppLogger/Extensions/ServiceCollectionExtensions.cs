04afc4cb1b0cfc468c1e3314121b75b0c73f4b71
DropBear <tkuchel@users.noreply.github.com>
2024-03-21 14:15:50 +1100

    Minor improvements.

--- DropBear.Codex.AppLogger/Extensions/ServiceCollectionExtensions.cs
+++ DropBear.Codex.AppLogger/Extensions/ServiceCollectionExtensions.cs
@@ -7,71 +7,70 @@ using Microsoft.Extensions.Logging;
 using ZLogger;
 using ZLogger.Providers;
 
-namespace DropBear.Codex.AppLogger.Extensions
+namespace DropBear.Codex.AppLogger.Extensions;
+
+/// <summary>
+///     Extensions for configuring application logging services.
+/// </summary>
+public static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// Extensions for configuring application logging services.
+    ///     Adds application logger services to the specified <see cref="IServiceCollection" />.
     /// </summary>
-    public static class ServiceCollectionExtensions
+    /// <param name="services">The <see cref="IServiceCollection" /> to add logging services to.</param>
+    /// <returns>The modified <see cref="IServiceCollection" /> with added logging services.</returns>
+    public static IServiceCollection AddAppLogger(this IServiceCollection services)
     {
-        /// <summary>
-        /// Adds application logger services to the specified <see cref="IServiceCollection"/>.
-        /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection"/> to add logging services to.</param>
-        /// <returns>The modified <see cref="IServiceCollection"/> with added logging services.</returns>
-        public static IServiceCollection AddAppLogger(this IServiceCollection services)
-        {
-            // Check if an ILogger<T> is already configured, considering any T
-            var loggerExists = services.Any(service =>
-                service.ServiceType.IsGenericType &&
-                service.ServiceType.GetGenericTypeDefinition() == typeof(ILogger<>));
+        // Check if an ILogger<T> is already configured, considering any T
+        var loggerExists = services.Any(service =>
+            service.ServiceType.IsGenericType &&
+            service.ServiceType.GetGenericTypeDefinition() == typeof(ILogger<>));
 
-            if (!loggerExists)
-                ConfigureZLogger(services);
+        if (!loggerExists)
+            ConfigureZLogger(services);
 
-            return AddLoggingAdapter(services);
-        }
-
-        /// <summary>
-        /// Adds the logging adapter service to the specified <see cref="IServiceCollection"/>.
-        /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection"/> to add the logging adapter service to.</param>
-        /// <returns>The modified <see cref="IServiceCollection"/> with added logging adapter service.</returns>
-        private static IServiceCollection AddLoggingAdapter(this IServiceCollection services)
-        {
-            services.AddSingleton(typeof(IAppLogger<>), typeof(AppLogger<>));
-            return services;
-        }
-
-        /// <summary>
-        /// Configures ZLogger logging services.
-        /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection"/> to add logging services to.</param>
-        private static void ConfigureZLogger(IServiceCollection services) =>
-            services.AddLogging(builder =>
-            {
-                builder.ClearProviders()
-                    .SetMinimumLevel(LogLevel.Debug)
-                    .AddZLoggerConsole(options =>
-                    {
-                        options.UseJsonFormatter(formatter =>
-                        {
-                            formatter.IncludeProperties = IncludeProperties.ParameterKeyValues;
-                        });
-                    })
-                    .AddZLoggerRollingFile(options =>
-                    {
-                        options.FilePathSelector = (timestamp, sequenceNumber) => new StringBuilder()
-                            .Append("logs/")
-                            .Append(timestamp.ToLocalTime().ToString("yyyy-MM-dd", CultureInfo.CurrentCulture))
-                            .Append('_')
-                            .Append(sequenceNumber.ToString("000", CultureInfo.CurrentCulture))
-                            .Append(".log")
-                            .ToString();
-
-                        options.RollingInterval = RollingInterval.Day;
-                        options.RollingSizeKB = 1024; // 1MB
-                    });
-            });
+        return AddLoggingAdapter(services);
     }
+
+    /// <summary>
+    ///     Adds the logging adapter service to the specified <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to add the logging adapter service to.</param>
+    /// <returns>The modified <see cref="IServiceCollection" /> with added logging adapter service.</returns>
+    private static IServiceCollection AddLoggingAdapter(this IServiceCollection services)
+    {
+        services.AddSingleton(typeof(IAppLogger<>), typeof(AppLogger<>));
+        return services;
+    }
+
+    /// <summary>
+    ///     Configures ZLogger logging services.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to add logging services to.</param>
+    private static void ConfigureZLogger(IServiceCollection services) =>
+        services.AddLogging(builder =>
+        {
+            builder.ClearProviders()
+                .SetMinimumLevel(LogLevel.Debug)
+                .AddZLoggerConsole(options =>
+                {
+                    options.UseJsonFormatter(formatter =>
+                    {
+                        formatter.IncludeProperties = IncludeProperties.ParameterKeyValues;
+                    });
+                })
+                .AddZLoggerRollingFile(options =>
+                {
+                    options.FilePathSelector = (timestamp, sequenceNumber) => new StringBuilder()
+                        .Append("logs/")
+                        .Append(timestamp.ToLocalTime().ToString("yyyy-MM-dd", CultureInfo.CurrentCulture))
+                        .Append('_')
+                        .Append(sequenceNumber.ToString("000", CultureInfo.CurrentCulture))
+                        .Append(".log")
+                        .ToString();
+
+                    options.RollingInterval = RollingInterval.Day;
+                    options.RollingSizeKB = 1024; // 1MB
+                });
+        });
 }
--- DropBear.Codex.AppLogger/LoggingStrategies/ZLoggerStrategy.cs
+++ DropBear.Codex.AppLogger/LoggingStrategies/ZLoggerStrategy.cs
@@ -1,42 +1,37 @@
 ﻿using DropBear.Codex.AppLogger.Interfaces;
 using Microsoft.Extensions.Logging;
 using ZLogger;
-using System;
 
-namespace DropBear.Codex.AppLogger.LoggingStrategies
+namespace DropBear.Codex.AppLogger.LoggingStrategies;
+
+/// <summary>
+///     Provides a logging strategy using ZLogger.
+/// </summary>
+/// <typeparam name="T">The type of logger.</typeparam>
+public class ZLoggerStrategy<T> : ILoggingStrategy
 {
+    private readonly ILogger<T> _logger;
+
     /// <summary>
-    /// Provides a logging strategy using ZLogger.
+    ///     Initializes a new instance of the <see cref="ZLoggerStrategy{T}" /> class.
     /// </summary>
-    /// <typeparam name="T">The type of logger.</typeparam>
-    public class ZLoggerStrategy<T> : ILoggingStrategy
-    {
-        private readonly ILogger<T> _logger;
+    /// <param name="logger">The logger instance.</param>
+    public ZLoggerStrategy(ILogger<T> logger) => _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ZLoggerStrategy{T}"/> class.
-        /// </summary>
-        /// <param name="logger">The logger instance.</param>
-        public ZLoggerStrategy(ILogger<T> logger)
-        {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
+    /// <inheritdoc />
+    public void LogDebug(string message) => _logger.ZLogDebug($"[D]: {message}");
 
-        /// <inheritdoc/>
-        public void LogDebug(string message) => _logger.ZLogDebug($"Processed: {message}");
+    /// <inheritdoc />
+    public void LogInformation(string message) => _logger.ZLogInformation($"[I]: {message}");
 
-        /// <inheritdoc/>
-        public void LogInformation(string message) => _logger.ZLogInformation($"Processed: {message}");
+    /// <inheritdoc />
+    public void LogWarning(string message) => _logger.ZLogWarning($"[W]: {message}");
 
-        /// <inheritdoc/>
-        public void LogWarning(string message) => _logger.ZLogWarning($"Processed: {message}");
+    /// <inheritdoc />
+    public void LogError(string message, Exception? exception = null) =>
+        _logger.ZLogError(exception, $"[E]: {message}");
 
-        /// <inheritdoc/>
-        public void LogError(string message, Exception? exception = null) =>
-            _logger.ZLogError(exception, $"Processed: {message}");
-
-        /// <inheritdoc/>
-        public void LogCritical(string message, Exception? exception = null) =>
-            _logger.ZLogCritical(exception, $"Processed: {message}");
-    }
+    /// <inheritdoc />
+    public void LogCritical(string message, Exception? exception = null) =>
+        _logger.ZLogCritical(exception, $"[C]: {message}");
 }

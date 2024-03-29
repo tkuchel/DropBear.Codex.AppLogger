2cd009e700e9ade134e4cbf7ccd5421f33dc32b7
DropBear <tkuchel@users.noreply.github.com>
2024-03-29 15:28:45 +1100

    Marked MessageTemplateFactory.cs as obsolete as this is being moved to the DropBear.Codex.Utilities library.

--- DropBear.Codex.AppLogger/Factories/MessageTemplateFactory.cs
+++ DropBear.Codex.AppLogger/Factories/MessageTemplateFactory.cs
@@ -1,37 +1,46 @@
-﻿using System.Collections.Concurrent;
-using Cysharp.Text;
+﻿using System.Buffers;
+using System.Collections.Concurrent;
+using System.Text;
+using Utf8StringInterpolation;
 
 namespace DropBear.Codex.AppLogger.Factories;
 
+[Obsolete("This class is obsolete and will be removed in a future version. Please use DropBear.Codex.Utilities.MessageTemplateManager instead.")]
 public static class MessageTemplateFactory
 {
     private static readonly ConcurrentDictionary<string, string> Templates = new(StringComparer.OrdinalIgnoreCase);
 
-    // ReSharper disable once MemberCanBePrivate.Global
     public static void RegisterTemplate(string templateId, string template)
     {
         if (!Templates.TryAdd(templateId, template))
             throw new InvalidOperationException($"A template with the ID '{templateId}' has already been registered.");
     }
 
-    public static string Format(string templateId, params object[] args)
+    public static byte[] FormatUtf8(string templateId, params object[] args)
     {
         if (!Templates.TryGetValue(templateId, out var template))
             throw new KeyNotFoundException($"No template found with the ID '{templateId}'.");
 
-        try
+        var bufferWriter = new ArrayBufferWriter<byte>();
+        // Ensuring Utf8StringWriter is disposed correctly using 'using' statement
+        using (var writer = Utf8String.CreateWriter(bufferWriter))
         {
-            return ZString.Format(template, args);
+            writer.AppendLiteral(template); // Assuming template is a literal part of the formatted message
+            foreach (var arg in args) writer.AppendFormatted(arg);
         }
-        catch (Exception ex)
-        {
-            throw new FormatException($"Error formatting the template '{templateId}'.", ex);
-        }
+
+        // No need to manually call Flush() since it's implicitly called by Dispose()
+        return bufferWriter.WrittenSpan.ToArray();
     }
 
-    // Optional: Method to bulk register templates, e.g., from configuration files or startup routines.
+    public static string FormatString(string templateId, params object[] args)
+    {
+        var utf8Bytes = FormatUtf8(templateId, args);
+        return Encoding.UTF8.GetString(utf8Bytes);
+    }
+
     public static void RegisterTemplates(Dictionary<string, string> templatesToRegister)
     {
-        foreach (var pair in templatesToRegister) RegisterTemplate(pair.Key, pair.Value);
+        foreach (var (key, value) in templatesToRegister) RegisterTemplate(key, value);
     }
 }
